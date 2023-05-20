349d052a906e4b498aed1a2970b5d6fc4bacdc5e
paulpriyam <ppaul2204@gmail.com>
2023-05-20 12:49:32 +0530

    resolved errors related to dependency.

--- app/src/main/java/com/example/runningapppl/MainActivity.kt
+++ app/src/main/java/com/example/runningapppl/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.runningapppl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.runningapppl.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
